92f572783634f76b19d95fa7b0b883bfb6d0e319
rutujdhodapkar <140371522+rutujdhodapkar@users.noreply.github.com>
2024-11-25 15:38:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,73 @@
-import asyncio
-import websockets
+from flask import Flask, render_template, session, jsonify
+from flask_socketio import SocketIO, join_room, leave_room, emit
+import os
+import random
 
-# List to hold all connected clients
-connected_clients = set()
+app = Flask(__name__)
+app.secret_key = os.urandom(24)
+socketio = SocketIO(app)
 
-async def handler(websocket, path):
-    # Add the new connection to the set of connected clients
-    connected_clients.add(websocket)
-    try:
-        # Wait for incoming messages from the client
-        async for message in websocket:
-            # Broadcast the message to all other connected clients
-            for client in connected_clients:
-                if client != websocket:
-                    await client.send(message)
-    except websockets.exceptions.ConnectionClosed:
-        print("A client disconnected.")
-    finally:
-        # Remove the client from the list of connected clients when they disconnect
-        connected_clients.remove(websocket)
+# In-memory store for connected users and the server's role
+users = []
+server = None  # No server initially
 
-# Start the WebSocket server
-async def main():
-    server = await websockets.serve(handler, "localhost", 8080)
-    print("Server started on ws://localhost:8080")
-    await server.wait_closed()
+# Web route to render the frontend
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-# Run the server
-asyncio.run(main())
+# Web route to get users list
+@app.route('/get_users', methods=['GET'])
+def get_users():
+    return jsonify(users=users)
+
+# SocketIO event when a user joins
+@socketio.on('connect')
+def on_connect():
+    # Add user to the list
+    username = f"User{random.randint(1000, 9999)}"
+    session['username'] = username
+    users.append(username)
+    emit('user_connected', {'username': username}, broadcast=True)
+
+    # Assign the first user as the server
+    if len(users) == 1:
+        global server
+        server = username
+        emit('server_assigned', {'server': server}, broadcast=True)
+
+    # Notify the user who the current server is
+    emit('server_update', {'server': server})
+
+# SocketIO event when a user sends a message
+@socketio.on('send_message')
+def handle_message(data):
+    message = data['message']
+    username = session['username']
+
+    # If the user is the server, broadcast the message to all users
+    if username == server:
+        emit('new_message', {'message': message, 'username': username}, broadcast=True)
+    else:
+        # Send the message only to the server
+        emit('new_message', {'message': message, 'username': username}, room=server)
+
+# SocketIO event when a user disconnects
+@socketio.on('disconnect')
+def on_disconnect():
+    username = session.get('username')
+    if username in users:
+        users.remove(username)
+
+    # If the server disconnects, assign a new server
+    if username == server:
+        global server
+        if users:
+            server = users[0]  # Assign the first user as the new server
+            emit('server_assigned', {'server': server}, broadcast=True)
+
+    # Broadcast the server update
+    emit('server_update', {'server': server}, broadcast=True)
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
